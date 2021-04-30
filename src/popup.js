f0738ef2b271d460f6115d596063ae887c5dbfac
Daniel Stephenson <daniel.stephenson@faithlife.com>
2021-04-30 12:33:23 -0700

    V2.0.0 - Emoji Reactions

--- src/popup.js
+++ src/popup.js
@@ -2,7 +2,7 @@ allEmojis = allEmojis;
 document.addEventListener(
 	"DOMContentLoaded",
 	function () {
-		var runButton = document.getElementById("emojis");
+		/*var runButton = document.getElementById("emojis");
 
 		function modifyDOM() {
 			var emojis = emojiList;
@@ -38,6 +38,72 @@ document.addEventListener(
 			chrome.tabs.executeScript({ code }, (results) => {
 				console.log(results[0]);
 			});
+		});*/
+		const favoriteInput = document.getElementById("favorite");
+		const addButton = document.getElementById("add");
+
+		function addFavoriteEmoji() {
+			const emojiName = favoriteInput.value;
+			const emoji = allEmojis[emojiName];
+			if (emoji) {
+				chrome.storage.sync.get(["favorites"], (items) => {
+					const { favorites } = items;
+					if (favorites[emojiName]) {
+						return;
+					}
+					chrome.storage.sync.set({ favorites: { ...(favorites ?? {}), [emojiName]: emoji } });
+
+					const emojiElement = document.createElement("div");
+					emojiElement.innerHTML = `<span class="emoji-text">:${emojiName}:</span><img class="emoji" src="${emoji}" style="width: 20px;"></img><span class="emoji-delete-x" data-emoji-name="${emojiName}">X</span>`;
+					emojiElement.classList.add("emoji-entry");
+
+					const emojiContainer = document.getElementById("saved-emojis");
+					emojiContainer.append(emojiElement);
+				});
+			} else {
+				console.error("No emoji with text ", emojiName);
+			}
+			favoriteInput.value = "";
+		}
+
+		function enterFavoriteEmoji(e) {
+			if (e.keyCode === 13) {
+				addFavoriteEmoji();
+			}
+		}
+
+		favoriteInput.addEventListener("keyup", enterFavoriteEmoji);
+		addButton.addEventListener("click", addFavoriteEmoji);
+
+		function deleteFavoritedEmoji(e) {
+			const xButton = e.target;
+			const emojiName = xButton.getAttribute("data-emoji-name");
+			const emojiEntry = xButton.closest(".emoji-entry");
+			chrome.storage.sync.get(["favorites"], (items) => {
+				const { favorites } = items;
+				if (favorites && favorites[emojiName]) {
+					delete favorites[emojiName];
+				}
+				chrome.storage.sync.set({ favorites: { ...(favorites ?? {}) } });
+				emojiEntry.remove();
+			});
+		}
+
+		chrome.storage.sync.get(["favorites"], (items) => {
+			const savedEmojis = Object.entries(items.favorites ?? {});
+			const emojiContainer = document.getElementById("saved-emojis");
+			for (let i = 0; i < savedEmojis.length; i++) {
+				const emojiName = savedEmojis[i][0];
+				const emoji = savedEmojis[i][1];
+				const emojiElement = document.createElement("div");
+				emojiElement.innerHTML = `<span class="emoji-text">:${emojiName}:</span><img class="emoji" src="${emoji}" style="width: 20px;"></img><span class="emoji-delete-x" data-emoji-name="${emojiName}">X</span>`;
+				emojiElement.classList.add("emoji-entry");
+				emojiElement.setAttribute("id", emojiName);
+				emojiContainer.append(emojiElement);
+
+				const emojiDelete = emojiElement.querySelector(".emoji-delete-x");
+				emojiDelete.addEventListener("click", deleteFavoritedEmoji);
+			}
 		});
 	},
 	false
